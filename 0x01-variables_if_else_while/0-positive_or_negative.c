df84627c9a33c92ed4d55eafea2ae1db06bd20ba
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-09-09 04:18:55 -0700

    c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Determines if numebr is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	
