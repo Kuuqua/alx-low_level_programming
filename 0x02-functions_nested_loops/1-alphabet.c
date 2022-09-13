a1dcfb7fba2df0580525fc626b3bd1fdd661b52d
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-09-13 07:25:57 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
+#include "main.h"
+/**
+ * print alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int i;
 
+	for (i =97; i <=122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
