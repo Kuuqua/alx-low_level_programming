f5540f6aa86850d814226572e06fae183a28b52c
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-09-13 00:43:51 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "putchar.h"
+/**
+ * main - print "_putchar" followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char putchar[] = "_putchar";
+	int i = 0;
+
+	while (putchar[i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
