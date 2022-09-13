b3a42bd628a397ea5dbeb3064e47059fa623476c
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-09-13 08:30:55 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,15 @@
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
+	char putchar[] = "_putchar";
+	int i = 0;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
+	while (putchar[i] != '\0')
 	{
-		_putchar(str[count]);
+		_putchar(putchar[i]);
+		i++;
 	}
 	_putchar('\n');
+
 	return (0);
 }
