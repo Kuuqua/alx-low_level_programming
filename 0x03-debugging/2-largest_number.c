8fc4f8733ac607939d5ef0a72a0fc2589543828e
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-09-14 15:25:30 -0700

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
+	if (a > b)
+	{
+		largest = a;
+	}
+	else
+	{
+		largest = b;
+	}
+	if (c > largest)
+	{
+		largest = c;
+	}
+	return (largest);
 }
