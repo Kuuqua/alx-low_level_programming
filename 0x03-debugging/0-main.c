092715bd4cad71441115c9082048174fc8afddc3
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-09-14 14:54:53 -0700

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main..h"
 
 /**
  * main - tests function that prints if integer is positive or negative
@@ -10,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
