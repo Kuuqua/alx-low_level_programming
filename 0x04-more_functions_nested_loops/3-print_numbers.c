9e42b30c030e8c21c676150eddf9b41ab38affc4
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-09-15 04:37:38 -0700

    error

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers from 0 to 9
+ * Description - you can only use _putchar twice
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (1 < 10)
+	{
+		_putchar(1 + '0');
+		i++;
+	}
+	_putchar('\n');
+}
