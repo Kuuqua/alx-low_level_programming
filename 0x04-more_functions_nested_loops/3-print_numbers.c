893833abbed66ae528f02b3553894dc13efb7b84
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-09-15 11:39:01 +0000

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - print the numbers from 0 to 9
- * Description - you can only use _putchar twice
+ * Description: You can only use _putchar twice
  */
 void print_numbers(void)
 {
 	int i;
 
 	i = 0;
-	while (1 < 10)
+	while (i < 10)
 	{
-		_putchar(1 + '0');
+		_putchar(i + '0');
 		i++;
 	}
 	_putchar('\n');
