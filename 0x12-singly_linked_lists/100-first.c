e3d1b1d110d4729650bcb9344a8f793307661167
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-10-20 13:20:06 +0000

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,11 @@
 #include "lists.h"
+/**
+ * bmain - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
