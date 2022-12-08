23f69d615deac5daf9487cf9a953ed754db24b60
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-12-08 08:31:06 +0000

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,46 +9,33 @@
  * Return: the pointer to dest.
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+  // Initialize the result string with all zeros
+  for (int i = 0; i < size_r; i++) {
+    r[i] = '0';
+  }
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
-		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
-	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-		*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
-	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+  // Compute the length of n1 and n2
+  int n1_len = strlen(n1);
+  int n2_len = strlen(n2);
+
+  // Compute the result by adding the digits of n1 and n2, one by one, starting from the least significant digit
+  int carry = 0;
+  int i = 0;
+  while (i < n1_len || i < n2_len || carry) {
+    int digit1 = i < n1_len ? n1[n1_len - 1 - i] - '0' : 0;
+    int digit2 = i < n2_len ? n2[n2_len - 1 - i] - '0' : 0;
+    int sum = digit1 + digit2 + carry;
+    r[size_r - 1 - i] = (sum % 10) + '0';
+    carry = sum / 10;
+    i++;
+  }
+
+  // Check if the result can fit in the result buffer
+  if (i > size_r - 1) {
+    return 0;
+  }
+
+  // Return a pointer to the result
+  return r;
 }
