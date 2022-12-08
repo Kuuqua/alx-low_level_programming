ea8c63012624c6de2e4058a7ebe31e72c50fd858
Kuuqua <110999355+Kuuqua@users.noreply.github.com>
2022-12-08 08:36:44 +0000

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,33 +1,47 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r) {
-  // Initialize the result string with all zeros
-  for (int i = 0; i < size_r; i++) {
-    r[i] = '0';
-  }
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+    // Determine the maximum length of the result
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    int max_len = len1 > len2 ? len1 : len2;
+    int carry = 0; // Keep track of any carry over from addition
 
-  // Compute the length of n1 and n2
-  int n1_len = strlen(n1);
-  int n2_len = strlen(n2);
+    // Make sure the result buffer is large enough to hold the result
+    if (max_len + 1 > size_r)
+    {
+        return 0;
+    }
 
-  // Compute the result by adding the digits of n1 and n2, one by one, starting from the least significant digit
-  int carry = 0;
-  int i = 0;
-  while (i < n1_len || i < n2_len || carry) {
-    int digit1 = i < n1_len ? n1[n1_len - 1 - i] - '0' : 0;
-    int digit2 = i < n2_len ? n2[n2_len - 1 - i] - '0' : 0;
-    int sum = digit1 + digit2 + carry;
-    r[size_r - 1 - i] = (sum % 10) + '0';
-    carry = sum / 10;
-    i++;
-  }
+    // Initialize the result buffer with null characters
+    memset(r, '\0', size_r);
 
-  // Check if the result can fit in the result buffer
-  if (i > size_r - 1) {
-    return 0;
-  }
+    // Iterate over the digits of each number from least significant to most significant
+    for (int i = 0; i < max_len; i++)
+    {
+        // Get the current digits from each number
+        int n1_digit = i < len1 ? n1[len1 - 1 - i] - '0' : 0;
+        int n2_digit = i < len2 ? n2[len2 - 1 - i] - '0' : 0;
 
-  // Return a pointer to the result
-  return r;
+        // Perform the addition and take into account the carry over
+        int sum = n1_digit + n2_digit + carry;
+        carry = sum / 10;
+        int result_digit = sum % 10;
+
+        // Add the result digit to the result buffer
+        r[max_len - 1 - i] = result_digit + '0';
+    }
+
+    // If there is a remaining carry over, add it to the result buffer
+    if (carry > 0)
+    {
+        r[0] = carry + '0';
+    }
+
+    // Return a pointer to the result buffer
+    return r;
 }
